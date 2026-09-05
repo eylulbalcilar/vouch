82bffab29638ddd98840326aa3d2ad1966948feb
eylulbalcilar <eylulbalcilar@gmail.com>
2026-09-06 00:04:08 +0200

    feat: add wagmi and rainbowkit providers with connect button

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { WagmiProvider } from 'wagmi'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { RainbowKitProvider, darkTheme} from '@rainbow-me/rainbowkit'
+import '@rainbow-me/rainbowkit/styles.css'
 import './index.css'
 import App from './App.tsx'
+import { config } from './wagmi.ts'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider
+        theme={darkTheme({
+            accentColor: "#0f172a",
+            accentColorForeground: "white",
+            borderRadius: "medium",
+          })}
+          >
+          <App/>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   </StrictMode>,
 )
+
+
